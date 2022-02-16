86d2b202d8e370a03bcf3ab4d2c5b6cbc5a0383f
mhendricks96 <mhendricks96@yahoo.com>
2022-02-15 20:03:19 -0800

    done with refactoring

---
+++ resty/src/components/results/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Results() {
+  return (
+    <section>
+      <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+    </section>
+  );
+}
+
+export default Results;
